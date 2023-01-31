9dc3b359787023be5c63351c1363468ff6907d76
Usirusen Ikon <lexicon048@gmail.com>
2023-01-31 17:10:34 +0100

    Write test for Controllers

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /posts' do
+    before(:each) { get '/users/2/posts' }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a list of posts' do
+      expect(response.body).to include('See my posts')
+    end
+
+    # If a correct template was rendered
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /posts/:id' do
+    before(:each) { get '/users/1/posts/1' }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns a post' do
+      expect(response.body).to include('Grey is silver')
+    end
+
+    # If a correct template was rendered
+    it 'renders the show template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
