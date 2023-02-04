91475ed040add70b41af568ac7e5a039e0ef09b3
Usirusen Ikon <lexicon048@gmail.com>
2023-02-04 15:37:27 +0100

    Create Forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,25 @@ class PostsController < ApplicationController
     @user = User.where(id: params[:user_id]).first
     @comments = Comment.includes(:author).where(id: params[:user_id]).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_posts_path
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:new]
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
