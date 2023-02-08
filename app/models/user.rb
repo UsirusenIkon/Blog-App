446816ae71a5387e8628863f0d044d3c058d3fa5
Usirusen Ikon <lexicon048@gmail.com>
2023-02-08 16:30:17 +0100

    Install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 # user
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  # before_action :authenticate_user!
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
