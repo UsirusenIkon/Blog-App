c02783eacb77e915b89357edb6d3537fa1c1f44c
Usirusen Ikon <lexicon048@gmail.com>
2023-02-09 20:06:41 +0100

    Effect route changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root "users#index"
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create, :new]
@@ -9,7 +11,6 @@ Rails.application.routes.draw do
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
