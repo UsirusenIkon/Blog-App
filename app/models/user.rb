98c22459f0dfbb1a95d9cc42bad98a92efa5fdd7
Usirusen Ikon <lexicon048@gmail.com>
2023-02-09 14:17:49 +0100

    Create Migrations for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 # user
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+  # :lockable, :timeoutable, :trackable and :omniauthable
   # before_action :authenticate_user!
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
